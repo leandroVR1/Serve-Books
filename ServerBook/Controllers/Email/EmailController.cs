3a9600e3ef50801b7d11910325b8b0d0dbc29606
Daniel_XD <danielmontiel1600@gmail.com>
2024-07-29 16:09:35 -0500

    Codigo base del envio de correos

---
+++ ServerBook/Controllers/Email/EmailController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using ServerBook.Models.Dtos;
+using ServerBook.Services.Interfaces;
+
+namespace ServerBook.Controllers.Email
+{
+    [ApiController]
+    [Route("api/v2/[controller]")]
+    public class EmailController : ControllerBase
+    {
+
+        private readonly IEmailRepository _emailRepository;
+        public EmailController(IEmailRepository emailRepository)
+        {
+            _emailRepository = emailRepository;
+        }
+        [HttpPost]
+        public IActionResult SendEmail(EmailDto request)
+        {
+            _emailRepository.SendEmail(request);
+            return Ok(request);
+        }
+    }
+}
